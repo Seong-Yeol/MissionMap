0bc5922f2168691337bf6d3c87c4d1cf479a6af1
Seong-Yeol <namsy013@gmail.com>
2016-07-02 14:36:32 +0900

    실행 성공 오예!

--- app/src/main/java/com/washswat/admin/missionmap/MapActivity.java
+++ app/src/main/java/com/washswat/admin/missionmap/MapActivity.java
@@ -2,6 +2,7 @@ package com.washswat.admin.missionmap;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.ViewGroup;
 
 import net.daum.mf.map.api.MapView;
@@ -13,11 +14,16 @@ public class MapActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+        Log.d("Gump","Called creater MapActivity");
 
         MapView mapView = new MapView(this);
-        mapView.setDaumMapApiKey("API_KEY");
+        Log.d("Gump","Success to create DaumMapView");
+        mapView.setDaumMapApiKey(getString(R.string.daum_api_key));
+        Log.d("Gump","Success to set DaumMapApiKey");
 
         ViewGroup mapViewContainer = (ViewGroup) findViewById(R.id.map_view);
         mapViewContainer.addView(mapView);
+
+        Log.d("Gump","Success to create MapActivity");
     }
 }
